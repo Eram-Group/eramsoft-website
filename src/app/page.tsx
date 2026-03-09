1b1ce70fca07e3b3a1b240fb4eea721917c8652b
yassmeen <yassmeensamy1@gmail.com>
2026-03-10 01:20:31 +0200

    keep grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import Navbar from "@/components/layout/Navbar";
 import Hero2 from "@/components/sections/Hero2";
 import Services from "@/components/sections/Services";
-import Solutions from "@/components/sections/Solutions";
-import ServicesAlt from "@/components/sections/ServicesAlt";
-import ServicesCards from "@/components/sections/ServicesCards";
 
 export default function Home() {
   return (
@@ -11,9 +8,6 @@ export default function Home() {
       <Navbar />
       <Hero2 />
       <Services />
-      <Solutions />
-      <ServicesAlt />
-      <ServicesCards />
     </>
   );
 }
