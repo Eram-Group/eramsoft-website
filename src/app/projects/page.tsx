8350b32388de5beb5857a38b294289604e841ef2
yassmeen <yassmeensamy1@gmail.com>
2026-03-14 15:29:43 +0200

    remove unused version

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState, useMemo } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { projects } from "@/data/projects";
-import "../projects-v5/projects-v5.css";
+import "./projects-v5.css";
 import "./projects-page.css";
 import "./projects-hero.css";
 
